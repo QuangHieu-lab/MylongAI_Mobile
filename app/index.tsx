e34c76b2ff4b266fab75e4857428519856293722
Hieu <cubul435@gmail.com>
2026-04-07 17:42:17 +0700

    changed name and modidfied app

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@/components/ui/button';
-import { Icon } from '@/components/ui/icon';
-import { Text } from '@/components/ui/text';
+import { Button } from '@/src/components/ui/button';
+import { Icon } from '@/src/components/ui/icon';
+import { Text } from '@/src/components/ui/text'
 import { Link, Stack } from 'expo-router';
 import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
